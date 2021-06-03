d30a36f6f717ab4278f08ca509d78532a0a4fbc5
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-02 21:27:31 -0400

    add text inputs to calculator

--- react-calculator/src/components/calculator.jsx
+++ react-calculator/src/components/calculator.jsx
@@ -5,6 +5,11 @@ const Calculate = () => {
     <div className="calc-container">
       <div className="calculator">
         <h1 className="title">React Calculator</h1>
+        <div className="input-container">
+          <input type="text" className="entry"></input>
+          <input type="text" className="entry"></input>
+          <input type="text" className="display"></input>
+        </div>
       </div>
     </div>
   )
