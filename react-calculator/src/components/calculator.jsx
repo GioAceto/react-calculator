f2b9ee89f635b09cd4e51942821d05589c4b7be1
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-05 11:32:53 -0400

    add operations select and equals button and centered css styling

--- react-calculator/src/components/calculator.jsx
+++ react-calculator/src/components/calculator.jsx
@@ -5,11 +5,18 @@ const Calculate = () => {
     <div className="calc-container">
       <div className="calculator">
         <h1 className="title">React Calculator</h1>
-        <div className="input-container">
-          <input type="text" className="entry"></input>
-          <input type="text" className="entry"></input>
-          <input type="text" className="display"></input>
-        </div>
+        <form className="input-container">
+          <input type="text" className="entry" />
+          <select id="operation">
+            <option id="addition">+</option>
+            <option id="subtraction">−</option>
+            <option id="multiplication">×</option>
+            <option id="division">÷</option>
+          </select>
+          <input type="text" className="entry" />
+          <button>=</button>
+          <input type="text" className="display" />
+        </form>
       </div>
     </div>
   )
