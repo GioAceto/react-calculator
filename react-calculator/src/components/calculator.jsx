c4286b4fe44e0c149f1fc4c1750e81fa43dafe7b
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-05 12:35:53 -0400

    add more styling for input boxes

--- react-calculator/src/components/calculator.jsx
+++ react-calculator/src/components/calculator.jsx
@@ -6,16 +6,16 @@ const Calculate = () => {
       <div className="calculator">
         <h1 className="title">React Calculator</h1>
         <form className="input-container">
-          <input type="text" className="entry" />
+          <input type="text" className="entry" placeholder="Enter a number" maxLength="12" />
           <select id="operation">
             <option id="addition">+</option>
             <option id="subtraction">−</option>
             <option id="multiplication">×</option>
             <option id="division">÷</option>
           </select>
-          <input type="text" className="entry" />
+          <input type="text" className="entry" placeholder="Enter a number" maxLength="12" />
           <button id="equals-btn">=</button>
-          <input type="text" className="display" />
+          <input type="text" className="display" disabled />
         </form>
       </div>
     </div>
