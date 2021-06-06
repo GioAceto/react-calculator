96755c8a1460906677fed45d6eaeac71dd894fd3
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-06 17:15:48 -0400

    change variable names and create new calculate function

--- react-calculator/src/components/calculator.jsx
+++ react-calculator/src/components/calculator.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
 
-const Calculate = () => {
+export default () => {
+  const [num1, setNum1] = useState('')
+  const [operation, setOperation] = useState('+')
+  const [num2, setNum2] = useState('')
+  const [result, setResult] = useState('')
+
+  const calculate = () => {
+
+  }
+
+  setResult('')
+
   return (
     <div className="calc-container">
       <div className="calculator">
         <h1 className="title">React Calculator</h1>
         <form className="input-container">
-          <input type="text" className="entry" placeholder="Enter a number" maxLength="12" />
-          <select id="operation">
+          <input type="text" className="entry" id="operand1" placeholder="Enter a number" maxLength="12" value={num1} onChange={event =>
+            setNum1(event.target.value)} />
+          <select id="operation" value={operation} onChange={event => setOperation(event.target.value)} >
             <option id="addition">+</option>
             <option id="subtraction">−</option>
             <option id="multiplication">×</option>
             <option id="division">÷</option>
           </select>
-          <input type="text" className="entry" placeholder="Enter a number" maxLength="12" />
-          <button id="equals-btn">=</button>
-          <input type="text" className="display" disabled />
+          <input type="text" className="entry" id="operand2" placeholder="Enter a number" maxLength="12" value={num2} onChange={event =>
+            setNum2(event.target.value)} />
+          <button id="equals-btn" type="button" onClick={calculate}>=</button>
+          <input type="text" className="display" disabled value={result} />
         </form>
       </div>
     </div>
   )
 }
-
-export default Calculate;
