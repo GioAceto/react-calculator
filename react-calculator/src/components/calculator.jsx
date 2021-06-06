926d320e35cb923b0ded68f79cb4d10105e117f5
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-06 17:21:20 -0400

    fix function structure

--- react-calculator/src/components/calculator.jsx
+++ react-calculator/src/components/calculator.jsx
@@ -6,8 +6,9 @@ export default () => {
   const [num2, setNum2] = useState('')
   const [result, setResult] = useState('')
 
-  const calculate = () => {
+  setResult('')
 
+  const calculate = () => {
     switch (operation) {
       case '+':
         return setResult(Number(num1) + Number(num2))
@@ -22,29 +23,25 @@ export default () => {
     }
   }
 
-}
-
-setResult('')
-
-return (
-  <div className="calc-container">
-    <div className="calculator">
-      <h1 className="title">React Calculator</h1>
-      <form className="input-container">
-        <input type="text" className="entry" id="operand1" placeholder="Enter a number" maxLength="12" value={num1} onChange={event =>
-          setNum1(event.target.value)} />
-        <select id="operation" value={operation} onChange={event => setOperation(event.target.value)} >
-          <option id="addition">+</option>
-          <option id="subtraction">−</option>
-          <option id="multiplication">×</option>
-          <option id="division">÷</option>
-        </select>
-        <input type="text" className="entry" id="operand2" placeholder="Enter a number" maxLength="12" value={num2} onChange={event =>
-          setNum2(event.target.value)} />
-        <button id="equals-btn" type="button" onClick={calculate}>=</button>
-        <input type="text" className="display" disabled value={result} />
-      </form>
+  return (
+    <div className="calc-container">
+      <div className="calculator">
+        <h1 className="title">React Calculator</h1>
+        <form className="input-container">
+          <input type="text" className="entry" id="operand1" placeholder="Enter a number" maxLength="12" value={num1} onChange={event =>
+            setNum1(event.target.value)} />
+          <select id="operation" value={operation} onChange={event => setOperation(event.target.value)} >
+            <option id="addition">+</option>
+            <option id="subtraction">−</option>
+            <option id="multiplication">×</option>
+            <option id="division">÷</option>
+          </select>
+          <input type="text" className="entry" id="operand2" placeholder="Enter a number" maxLength="12" value={num2} onChange={event =>
+            setNum2(event.target.value)} />
+          <button id="equals-btn" type="button" onClick={calculate}>=</button>
+          <input type="text" className="display" disabled value={result} />
+        </form>
+      </div>
     </div>
-  </div>
-)
+  )
 }
